337f1b9274b2aa9cdd4ef710c55c4b1b0c3d6b30
ShashikaVirajh <shashikasvka@gmail.com>
2022-07-21 00:40:55 +0530

    Sentry (#2)
    Sentry and slack integrated

--- src/pages/dashboard/stats/stats.page.tsx
+++ src/pages/dashboard/stats/stats.page.tsx
@@ -3,22 +3,23 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { fetchStatsRequest } from 'store/stats/stats.actions';
 import { StatsContainer } from 'components/stats';
-import ChartsContainer from 'components/charts/chart-container/chart-container.component';
 import { selectMonthlyApplications } from 'store/stats/stats.selector';
+
+import ChartsContainer from 'components/charts/chart-container/chart-container.component';
 
 const Stats: FC = () => {
   const dispatch = useDispatch();
-
-  const monthlyApplications = useSelector(selectMonthlyApplications);
 
   useEffect(() => {
     dispatch(fetchStatsRequest());
   }, [dispatch]);
 
+  const monthlyApplications = useSelector(selectMonthlyApplications);
+
   return (
     <>
       <StatsContainer />
-      <ChartsContainer data={monthlyApplications} />
+      <ChartsContainer data={monthlyApplications} />ß
     </>
   );
 };
